0e7514adc696f0c23d7d169231da8f0b47220ad8
hesousa <hugo161299@gmail.com>
2023-11-17 15:24:28 -0300

    Implementado DB H2 e JPA

--- SpringCourse/src/main/java/com/estudando/SpringCourse/entities/User.java
+++ SpringCourse/src/main/java/com/estudando/SpringCourse/entities/User.java
@@ -4,11 +4,16 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
